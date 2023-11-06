578a43574be1995154498b9c74f9f574b602b8c8
David Manuel <davidmagarcia95@gmail.com>
2023-11-06 16:58:55 +0100

    Comentario de rutas

--- 1Pruebareact-node/server/src/app.js
+++ 1Pruebareact-node/server/src/app.js
@@ -5,6 +5,7 @@ const databaseConnect = require('../database/database');
 require('dotenv').config();
 const userRoutes = require('./routes/user.routes')
 app.use(express.json());
+//Primero lo paso a json y luego ya uso las rutas
 app.use(userRoutes);
 
 app.listen(process.env.PORT, () => {
