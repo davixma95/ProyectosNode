8d2f2063540a15e21f7f8aaa13573e9de1fe0814
David Manuel <davidmagarcia95@gmail.com>
2023-11-07 15:08:53 +0100

    Subo el find y findById

--- 1Pruebareact-node/server/src/controllers/user.controller.js
+++ 1Pruebareact-node/server/src/controllers/user.controller.js
@@ -2,19 +2,29 @@ const userModel = require("../schemes/user.scheme");
 
 const controller = {};
 
-controller.createUser = (req,res) => {
-    try{
-        const { username, password, email } = req.body;
-        const newUser = new userModel({
+
+controller.getAllUsers = async (req, res) => {
+    try {
+        const allUsers = await userModel.find();
+        // const allUsers = await userModel.findById(req.params.id);
+        res.status(200).send(allUsers);
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+
+controller.createUser = async(req,res) => {
+    const { username, password, email } = req.body;
+    const newUser = new userModel({
         username,
         password,
         email,
     })
-        newUser.save();
-   
-
+    try{  
+        await newUser.save();
     } catch(error){
-        console.error('El error es ',error)
+        console.error('El error es ',error);
     }
 };
 
